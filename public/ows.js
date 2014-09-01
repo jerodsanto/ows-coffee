462208c62be8d8825639df807a0f1f66f7f40fcc
Jerod Santo <jerod.santo@gmail.com>
2014-09-01 18:14:18 -0500

    add faces, ability to play sounds, analytics, etc.

--- public/ows.js
+++ public/ows.js
@@ -1,3 +1,8 @@
+(function(fn){
+    if (!fn.map) fn.map=function(f){var r=[];for(var i=0;i<this.length;i++)r.push(f(this[i]));return r}
+    if (!fn.filter) fn.filter=function(f){var r=[];for(var i=0;i<this.length;i++)if(f(this[i]))r.push(this[i]);return r}
+})(Array.prototype);
+
 this.OWS = {
   hasClass: function(element, className) {
     return (" " + element.className + " ").indexOf(" " + className + " ") > -1;
@@ -43,43 +48,83 @@ this.OWS = {
   },
 
   updateButton: function() {
+    var self = this;
+
     this.updateSelected();
-    document.getElementById("selected-count").innerHTML = this.selected;
-    document.getElementById("selected-dollars").innerHTML = "$" + this.totalDollars;
+    this.selectedDollars.innerHTML = "$" + this.totalDollars;
+    this.selectedCount.innerHTML = this.selected;
+
+    this.addClass(this.button, "pulse");
+    setTimeout(function() {
+      self.removeClass(self.button, "pulse");
+    }, 1000);
+  },
+
+  playSound: function(name) {
+    var audio = document.getElementById(name);
+    if (audio) {
+      audio.play();
+    } else {
+      var audio = document.createElement("audio");
+      audio.id = name;
+      audio.autoplay = "autoplay";
+      audio.innerHTML = "<source src='audio/"+name+".mp3'><source src='audio/"+name+".ogg'>";
+      document.body.appendChild(audio);
+    }
   },
 
   init: function(stripeKey) {
     var self = this;
+    var i = 0, j = 0;
 
     this.stripe = StripeCheckout.configure({
       key: stripeKey,
-      image: "logo.png",
+      image: "images/logo.png",
       token: this.charge
     })
+
+    this.selectedCount = document.getElementById("selected-count");
+    this.selectedDollars = document.getElementById("selected-dollars");
+    this.fancies = document.getElementsByClassName("fancy-text");
+
+    for (i = 0; i < this.fancies.length; i++) {
+      var letters = this.fancies[i].innerHTML.split("");
+
+      this.fancies[i].innerHTML = letters.map(function(letter) {
+        ++j;
+        return "<span class='char"+j+"'>"+letter+"</span>";
+      }).join("");
+    }
 
     this.boxes = document.getElementsByClassName("box");
     this.button = document.getElementById("coffee");
 
-    for (var i = 0; i < this.boxes.length; i++) {
-      this.boxes[i].onmouseover = function() {
+    for (i = 0; i < this.boxes.length; i++) {
+      var box = this.boxes[i];
+
+      box.onmouseover = function() {
         if (!self.hasClass(this, "selected")) {
           self.addClass(this, "hovered");
+          ga("send", "face", "hover", box.id);
         }
       }
 
-      this.boxes[i].onmouseout = function() {
+      box.onmouseout = function() {
         if (!self.hasClass(this, "selected")) {
           self.removeClass(this, "hovered");
         }
       }
 
-      this.boxes[i].onclick = function() {
+      box.onclick = function() {
         if (self.hasClass(this, "selected")) {
           if (self.selected > 1) {
             self.removeClass(this, "selected");
+            self.removeClass(this, "hovered");
+            ga("send", "face", "deselect", box.id);
           }
         } else {
           self.addClass(this, "selected");
+          ga("send", "face", "select", box.id);
         }
 
         self.updateButton();
@@ -96,6 +141,14 @@ this.OWS = {
         panelLabel: "Send {{amount}}"
       })
     }
+
+    // initialize a randomly selected box
+    this.addClass(this.boxes[Math.floor(Math.random() * 3)], "selected");
+
+    // scroll to bottom after a second, to ensure buy button is seen
+    setTimeout(function() {
+      window.scrollTo(0,document.body.scrollHeight);
+    }, 1500);
 
     this.updateButton();
   }
